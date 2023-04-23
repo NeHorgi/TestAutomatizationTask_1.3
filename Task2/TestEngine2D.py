899b69ca1adcf5b5bd37f47125d789c27144ca30
NeHorgi <119290694+NeHorgi@users.noreply.github.com>
2023-04-23 18:11:14 +0200

    Add files via upload

--- Task2/TestEngine2D.py
+++ Task2/TestEngine2D.py
@@ -3,76 +3,62 @@ import pytest
 import Engine2D
 
 
+@pytest.fixture(scope='function')
+def create_test_engine():
+    test_engine = Engine2D.Engine2D()
+    return test_engine
+
+
+@pytest.fixture(scope='function')
+def create_broken_figure():
+
+    class BrokenFigure(Engine2D.Triangle):
+
+        def draw(self):
+            super().draw()
+            raise f'Error'
+
+    broken_figure = BrokenFigure(10, 10, 10)
+
+    return broken_figure
+
+
 class TestUnit:
 
-    def test_create_a_figures(self):
-        test_figure1 = Engine2D.Triangle(10, 10, 10)
-        test_figure2 = Engine2D.Rectangle(10, 10)
-        test_figure3 = Engine2D.Circle((0, 0), 10)
-
-        test_figures = [test_figure1, test_figure2, test_figure3]
-
-        for test_figure in test_figures:
-            assert isinstance(test_figure, Engine2D.Figure)
-
-    def test_create_not_a_figure(self):
-        with pytest.raises(TypeError):
-            Engine2D.Triangle(10, 10)
-
-    def test_add_a_figure_on_canvas(self):
-        test_engine = Engine2D.Engine2D()
+    def test_engines_add_method(self, create_test_engine):
         test_figure = Engine2D.Triangle(10, 10, 10)
 
-        test_engine.add(test_figure)
+        create_test_engine.add(test_figure)
 
-        assert test_engine.canvas[0] == test_figure
+        assert create_test_engine.canvas
 
-    def test_correct_type_of_added_figure(self):
-        test_engine = Engine2D.Engine2D()
+    def test_engines_add_not_a_figure(self, create_test_engine):
+        create_test_engine.add(0)
+
+        assert not create_test_engine.canvas
+
+    def test_engine_set_color(self, create_test_engine):
+        create_test_engine.set_color('green')
+
+        assert create_test_engine.color == 'green'
+
+    def test_empty_canvas(self, create_test_engine):
+        create_test_engine.add(Engine2D.Triangle(10, 10, 10))
+        create_test_engine.draw()
+
+        assert not create_test_engine.canvas
+
+    def test_draw_broken_figure(self, create_test_engine, create_broken_figure):
+        create_test_engine.add(create_broken_figure)
+        try:
+            create_test_engine.draw()
+        except Exception:
+            assert len(create_test_engine.canvas) == 1
+
+    def test_correct_name_of_figure(self):
         test_figure = Engine2D.Triangle(10, 10, 10)
 
-        test_engine.add(test_figure)
+        assert test_figure.name == 'Triangle'
 
-        assert isinstance(test_engine.canvas[0], Engine2D.Triangle)
 
-    def test_add_not_a_figure_on_canvas(self):
-        test_engine = Engine2D.Engine2D()
-        test_engine.add(0)
-
-        assert not test_engine.canvas
-
-    def test_adding_a_lot_of_figures(self):
-        test_engine = Engine2D.Engine2D()
-        test_figure1 = Engine2D.Triangle(10, 10, 10)
-        test_figure2 = Engine2D.Rectangle(10, 10)
-        test_figure3 = Engine2D.Circle((0, 0), 10)
-
-        test_figures = [test_figure1, test_figure2, test_figure3]
-
-        for test_figure in test_figures:
-            test_engine.add(test_figure)
-
-        test_engine.add(0)
-
-        assert len(test_engine.canvas) == 3
-
-    def test_change_color(self):
-        test_engine = Engine2D.Engine2D()
-        test_engine.set_color('green')
-        test_engine.set_color('blue')
-
-        assert test_engine.color == 'blue'
-
-    def test_draw_empty_canvas(self):
-        test_engine = Engine2D.Engine2D()
-
-        assert test_engine.draw() is None
-
-    def test_correct_attributes_of_figure_on_canvas(self):
-        test_engine = Engine2D.Engine2D()
-        test_figure = Engine2D.Circle((0, 0), 5)
-
-        test_engine.add(test_figure)
-
-        assert str(test_engine.canvas[0]) == f'Circle with center in (0, 0) and 5 radius'
 
