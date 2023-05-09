e9099a35f27832d2b4ab6a7e50297492b17bfac9
NeHorgi <119290694+NeHorgi@users.noreply.github.com>
2023-05-09 22:15:51 +0200

    Add files via upload

--- CivMap/map_from_civ.py
+++ CivMap/map_from_civ.py
@@ -23,6 +23,8 @@ class GenerateMapFromCiv:
             for j in range(len(pangea[i])):
                 if (len(pangea) // 2.5) <= i < len(pangea) - len(pangea) // 2.5:
                     if (len(pangea) // 2.5) <= j < len(pangea) - len(pangea) // 2.5:
+                        if not self.ground_percent:
+                            break
                         pangea[i][j] = self.ground_percent.pop()
 
         for i in range(len(pangea)):
@@ -79,6 +81,72 @@ class GenerateMapFromCiv:
 
         return self.map
 
+    def create_continents(self):
+        first_continent = []
+        second_continent = []
+        lands = self.ground_percent
+
+        count_of_lands_first_continent = len(self.ground_percent) // 2
+        count_of_lands_second_continent = len(self.ground_percent) - count_of_lands_first_continent
+
+        a = self.x // 2
+        b = self.x - a
+        c = self.y
+
+        for _ in range(c):
+            row = []
+            for j in range(a):
+                row.append('~')
+            first_continent.append(row)
+
+        for i in range(len(first_continent)):
+            for j in range(len(first_continent[i])):
+                if (len(first_continent) // 3) <= i <= len(first_continent) - len(first_continent) // 2.5:
+                    if j == len(first_continent[i]) // 2:
+                        if not count_of_lands_first_continent:
+                            break
+                        first_continent[i][j] = self.ground_percent.pop(0)
+                        count_of_lands_first_continent -= 1
+
+        while count_of_lands_first_continent:
+            for i in range(len(first_continent)):
+                for j in range(len(first_continent[i])):
+                    chance = random.randint(0, 2)
+                    if not count_of_lands_first_continent:
+                        break
+                    if not chance:
+                        first_continent[i][j] = self.ground_percent.pop(0)
+                        count_of_lands_first_continent -= 1
+
+        for _ in range(c):
+            row = []
+            for j in range(b):
+                row.append('~')
+            second_continent.append(row)
+
+        for i in range(len(second_continent)):
+            for j in range(len(second_continent[i])):
+                if (len(second_continent) // 3) <= i <= len(second_continent) - len(second_continent) // 2.5:
+                    if j == len(second_continent[i]) // 2:
+                        if not self.ground_percent:
+                            break
+                        second_continent[i][j] = self.ground_percent.pop(0)
+                        count_of_lands_second_continent -= 1
+
+        while count_of_lands_second_continent:
+            for i in range(len(second_continent)):
+                for j in range(len(second_continent[i])):
+                    chance = random.randint(0, 2)
+                    if not count_of_lands_second_continent:
+                        break
+                    if not chance:
+                        second_continent[i][j] = self.ground_percent.pop(0)
+                        count_of_lands_second_continent -= 1
+
+        for i in range(len(self.map)):
+            self.map[i] = first_continent[i] + second_continent[i]
+
+        return self.map
 
 
 if __name__ == '__main__':
@@ -87,5 +155,7 @@ if __name__ == '__main__':
     CurrentMap = GenerateMapFromCiv(a, b)
     #for i in CurrentMap.create_pangea():
     #    print(i)
-    for i in CurrentMap.create_islands():
-        print(i)
+    #for i in CurrentMap.create_islands():
+    #    print(i)
+    #for i in CurrentMap.create_continents():
+    #    print(i)
